ae0cc1adc8ca405f1fc4055acc7d93a8e8cda2bd
Nils Krüger <36828049+n-krueger@users.noreply.github.com>
2019-04-13 17:18:25 +0200

    Add threaded merge sort and stdlib comparison

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate crossbeam;
+extern crate rand;
+extern crate time;
+extern crate num_cpus;
+
+use rand::Rng;
+
 fn swap(array: &mut [i32], i: usize, j: usize)  {
     let temp = array[i];
     array[i] = array[j];
@@ -58,21 +65,102 @@ fn merge_sort(array: &[i32]) -> Vec<i32> {
         }
     } else {
         let middle = length / 2;
-        let low_halve = merge_sort(&array[0..middle]);
-        let high_halve = merge_sort(&array[middle..length]);
+        let low_slice = &array[0..middle];
+        let high_slice = &array[middle..length];
+        let low_halve = merge_sort(low_slice);
+        let high_halve = merge_sort(high_slice);
+        return merge(&low_halve, &high_halve);
+    }
+}
+
+fn merge_sort_threaded(array: &[i32], nthreads: usize) -> Vec<i32> {
+    let length = array.len();
+
+    if length < 2 {
+        return array.to_vec();
+    } else if length == 2 {
+        if array[0] > array[1] {
+            return vec![array[1], array[0]];
+        } else {
+            return array.to_vec();
+        }
+    } else {
+        let middle = length / 2;
+        let low_slice = &array[0..middle];
+        let high_slice = &array[middle..length];
+        let low_halve: Vec<i32>;
+        let high_halve: Vec<i32>;
+
+        if nthreads > 0 {
+            let result = crossbeam::scope(
+                |scope| {
+                    let high_nthreads = nthreads / 2;
+                    let handle = scope.spawn(
+                        move |_| {merge_sort_threaded(low_slice, high_nthreads)}
+                    );
+                    let high_halve = merge_sort_threaded(
+                        high_slice,
+                        nthreads - high_nthreads
+                    );
+                    let low_halve = handle.join().unwrap();
+
+                    return (low_halve, high_halve)
+                }
+            ).unwrap();
+            low_halve = result.0;
+            high_halve = result.1;
+        } else {
+            low_halve = merge_sort_threaded(low_slice, nthreads);
+            high_halve = merge_sort_threaded(high_slice, nthreads);
+        }
 
         return merge(&low_halve, &high_halve);
     }
 }
 
 fn main() {
-    let numbers = [12, 24, 1, 2, 0, -54, 2, 35, 2, 1, -9999, 18];
-    print!("Original:\n{:?}\n\n", numbers);
+    let mut rng = rand::thread_rng();
+    let mut numbers = [0; 1000000];
+    for i in 0..numbers.len() {
+        numbers[i] = rng.gen();
+    }
+    let mut before: u64;
+    let mut after: u64;
+    let mut in_ms: u64;
 
-    let mut bubble_sorted = numbers;
-    bubble_sort(&mut bubble_sorted);
-    print!("Bubble Sort:\n{:?}\n\n", bubble_sorted);
+    if numbers.len() < 10000 {
+        let mut _bubble_sorted = numbers;
+        before = time::precise_time_ns();
+        bubble_sort(&mut _bubble_sorted);
+        after = time::precise_time_ns();
+        in_ms = (after - before) / 1000000;
+        println!("Bubble Sort: {}ms", in_ms);
+    }
 
-    let merge_sorted = merge_sort(&numbers);
-    println!("Merge Sort:\n{:?}\n\n", merge_sorted);
+    before = time::precise_time_ns();
+    let _merge_sorted = merge_sort(&numbers);
+    after = time::precise_time_ns();
+    in_ms = (after - before) / 1000000;
+    println!("Merge Sort: {}ms", in_ms);
+
+    before = time::precise_time_ns();
+    let _merge_sorted_threaded = merge_sort_threaded(
+        &numbers,
+        num_cpus::get()
+    );
+    after = time::precise_time_ns();
+    in_ms = (after - before) / 1000000;
+    println!("Merge Sort (threaded): {}ms", in_ms);
+
+    before = time::precise_time_ns();
+    let _stdlib_sorted = numbers.sort();
+    after = time::precise_time_ns();
+    in_ms = (after - before) / 1000000;
+    println!("stdlib: {}ms", in_ms);
+
+    before = time::precise_time_ns();
+    let _stdlib_sorted_unstable = numbers.sort_unstable();
+    after = time::precise_time_ns();
+    in_ms = (after - before) / 1000000;
+    println!("stdlib (unstable): {}ms", in_ms);
 }
